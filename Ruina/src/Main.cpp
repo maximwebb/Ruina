515a53de5e2c53bdc074b748446a0d29166f774c
Maxim Webb <maximwebb@gmail.com>
2020-07-02 11:16:08 +0100

    Separate Vertex and Fragment shader into individual files, add debugging messages.

--- Ruina/src/Main.cpp
+++ Ruina/src/Main.cpp
@@ -3,6 +3,43 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <sstream>
+#include <fstream>
+
+static const std::string GetSeverity(GLenum severity) {
+	if (severity == GL_DEBUG_SEVERITY_HIGH_AMD) {
+		return "HIGH";
+	}
+	else if (severity == GL_DEBUG_SEVERITY_MEDIUM_AMD) {
+		return "MEDIUM";
+	}
+	else if (severity == GL_DEBUG_SEVERITY_LOW_AMD) {
+		return "LOW";
+	}
+	
+	return "NONE";
+}
+
+static auto APIENTRY LogErrorInfo(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+	const std::string severity_name = GetSeverity(severity);
+	if (severity_name != "NONE") {
+		std::cout << "[OpenGL Error] Severity: " << severity_name << std::endl << message << std::endl;
+		__debugbreak();
+	}
+	return 0;
+}
+
+static std::string ParseShader(const std::string& filepath) {
+	std::ifstream stream(filepath);
+	std::string line;
+	std::stringstream ss;
+
+	while (getline(stream, line)) {
+		ss << line << '\n';
+	}
+
+	return ss.str();
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source) {
 	unsigned int id = glCreateShader(type);
@@ -15,7 +52,7 @@ static unsigned int CompileShader(unsigned int type, const std::string& source) 
 	if (result == GL_FALSE) {
 		int length;
 		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-		char* message = (char*)alloca(length * sizeof(char));
+		char* message = (char*)_malloca(length * sizeof(char));
 
 		glGetShaderInfoLog(id, length, &length, message);
 		std::cout << "Failed to compile " <<
@@ -30,11 +67,13 @@ static unsigned int CompileShader(unsigned int type, const std::string& source) 
 
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
 	unsigned int program = glCreateProgram();
+
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
 	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 	
 	glAttachShader(program, vs);
 	glAttachShader(program, fs);
+	
 
 	glLinkProgram(program);
 	glValidateProgram(program);
@@ -69,47 +108,45 @@ int main(void)
 		std::cout << "Error initialising GLEW." << std::endl;
 	}
 
+	glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback((GLDEBUGPROC)LogErrorInfo, nullptr);
+
 	/*std::array<float, 6> data = {
 		-0.5f, -0.5f,
 		 0.0f,  0.7f,
 		 0.5f, -0.5f
 	};*/
 
-	float data[6] = {
-		-0.5f, -0.5f,
-		 0.0f,  0.7f,
-		 0.5f, -0.5f
+	float data[] = {
+		-0.5f, -0.5f, // 0
+		 0.5f, -0.5f, // 1
+		 0.5f,  0.5f, // 2
+		-0.5f,  0.5f  // 3
 	};
 
 	unsigned int buffer1;
 	glGenBuffers(1, &buffer1);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer1);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), data, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"\n"
-		"void main() {\n"
-		"	gl_Position = position;\n"
-		"}\n";
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
 
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;\n"
-		"\n"
-		"void main() {\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-		"}\n";
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+	
+	std::string vertexShader = ParseShader("res/shaders/Vertex.shader");
+	std::string fragmentShader = ParseShader("res/shaders/Fragment.shader");
 
 	unsigned int shader = CreateShader(vertexShader, fragmentShader);
 	glUseProgram(shader);
-
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -117,7 +154,7 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -125,6 +162,8 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
